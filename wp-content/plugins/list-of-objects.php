43c14dec986334b6c7f422b871dad8b16bac6229
AryaAppaji <maappaji111@gmail.com>
2024-01-01 17:05:11 +0530

    modified api plugins

--- wp-content/plugins/list-of-objects.php
+++ wp-content/plugins/list-of-objects.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * Plugin Name: Test GET SPECIFIC API
+     * Plugin Name: Test List of objects API
      * Description: This is used to test the GET API
      * Author: Arya Appaji
      * Version: 1.0
@@ -8,7 +8,7 @@
     
     function get_response(){
         ob_start();
-        $data = wp_remote_retrieve_body(wp_remote_get('https://api.restful-api.dev/objects/7'));
+        $data = wp_remote_retrieve_body(wp_remote_get('https://api.restful-api.dev/objects/?id=3&id=5&id=10'));
         echo $data;
         ob_end_flush();
     }
