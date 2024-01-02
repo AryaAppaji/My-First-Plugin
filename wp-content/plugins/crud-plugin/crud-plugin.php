f5ce692678adb45bfc12d8d622e3eb52a6bdf56e
AryaAppaji <maappaji111@gmail.com>
2024-01-02 23:30:18 +0530

    Created basic CRUD plugin

--- wp-content/plugins/crud-plugin/crud-plugin.php
+++ wp-content/plugins/crud-plugin/crud-plugin.php
@@ -1,3 +1,37 @@
 <?php
+   /**
+    * Plugin Name: CRUD Plugin
+    * Description: This plugin is used to demonstrate CRUD operation on database
+    * Version: 1.0 
+    * Author: Arya Appaji
+    */
     
+    if(!defined("ABSPATH")){
+        die;
+    }
+
+    class CrudPlugin{
+        function displayOptions(){
+            ob_start();
+?>
+            <div>
+                <label for="Create">Create</label>
+                <input type="radio" name="Operation" id="Create">
+
+                <label for="Read">Read</label>
+                <input type="radio" name="Operation" id="Read">
+
+                <label for="Update">Update</label>
+                <input type="radio" name="Operation" id="Update">
+
+                <label for="Delete">Delete</label>
+                <input type="radio" name="Operation" id="Delete">
+            </div>
+<?php
+            ob_end_flush();
+        }
+    }
+
+    $obj = new CrudPlugin();
+    add_shortcode("crud-select", array($obj, "displayOptions"));
 ?>
