5bc32f5cc5fcca389cbcb8e203296e4cf36dd11c
AryaAppaji <maappaji111@gmail.com>
2024-01-03 16:52:58 +0530

    created a plugin for crud operation

--- wp-content/plugins/crud-plugin/crud-plugin.php
+++ wp-content/plugins/crud-plugin/crud-plugin.php
@@ -1,37 +1,67 @@
 <?php
-   /**
-    * Plugin Name: CRUD Plugin
-    * Description: This plugin is used to demonstrate CRUD operation on database
-    * Version: 1.0 
-    * Author: Arya Appaji
-    */
-    
-    if(!defined("ABSPATH")){
+    /**
+     * Plugin Name: CRUD Plugin
+     * Description: This plugin is used to demonstrate CRUD operation on the database
+     * Version: 1.0 
+     * Author: Arya Appaji
+     */
+
+    if (!defined("ABSPATH")) {
         die;
     }
 
-    class CrudPlugin{
-        function displayOptions(){
+    class CrudPlugin
+    {
+        function displayOptions()
+        {
             ob_start();
-?>
+            ?>
             <div>
-                <label for="Create">Create</label>
-                <input type="radio" name="Operation" id="Create">
-
-                <label for="Read">Read</label>
-                <input type="radio" name="Operation" id="Read">
-
-                <label for="Update">Update</label>
-                <input type="radio" name="Operation" id="Update">
-
-                <label for="Delete">Delete</label>
-                <input type="radio" name="Operation" id="Delete">
+                <select name="Operation" id="oper" onchange="showHideForm()">
+                    <option value="None">None</option>
+                    <option value="Create">Create</option>
+                    <option value="Read">Read</option>
+                    <option value="Update">Update</option>
+                    <option value="Delete">Delete</option>
+                </select>
+                <br>
             </div>
-<?php
+            <?php
+            ob_end_flush();
+        }
+
+        function showCrudForm()
+        {
+            ob_start();
+            ?>
+            <form action="" method="post" id="crudForm" style="display:none;">
+                <input type="text" name="name" placeholder="Enter Name"><br><br>
+                <input type="email" name="mail" placeholder="Enter Email Address"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+            <?php
             ob_end_flush();
         }
     }
 
     $obj = new CrudPlugin();
     add_shortcode("crud-select", array($obj, "displayOptions"));
+    add_shortcode("show-form", array($obj, "showCrudForm"));
 ?>
+
+<script>
+    function showHideForm() {
+        var opt = document.getElementById("oper");
+        var form = document.getElementById("crudForm");
+        var h1 = document.getElementById("heading");
+
+        if (opt.value === "Create") {
+            form.style.display = "block";
+        } else {
+            form.style.display = "none";
+        }
+    }
+
+    // Initial check on page load
+    showHideForm();
+</script>
